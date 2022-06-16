950f625b90f9dc7bd9f31a596739058df8729502
ruyman <ruyman.azzollini@gmail.com>
2022-06-16 20:34:41 +0200

    debug uart_test.c

--- src/FlySkyIBus/uart_test.c
+++ src/FlySkyIBus/uart_test.c
@@ -8,13 +8,13 @@
 #include <stdio.h>
 #include "pico/stdlib.h"
 #include "hardware/uart.h"
-//#include "hardware/irq.h"
+#include "hardware/irq.h"
 
 #define UART_ID uart1
 #define BAUD_RATE 115200
 #define DATA_BITS 8
 #define STOP_BITS 1
-#define PARITY    UART_PARITY_NONE
+#define PARITY UART_PARITY_NONE
 
 #define BUF_LEN 32
 
@@ -23,17 +23,23 @@
 //#define UART_TX_PIN 0
 #define UART_RX_PIN 5
 
-//static int chars_rxed = 0;
+static int bytes_rxed = 0;
+//static uint8_t buffer;
 
-/*
 // RX interrupt handler
-void on_uart_rx() {
+//void on_uart_rx(uint8_t *buf, uint16_t len) {
+void on_uart_rx(void) {
+    
+    uint8_t buffer[BUF_LEN];
+
     while (uart_is_readable(UART_ID)) {
-        uint8_t ch = uart_getc(UART_ID);
-        chars_rxed++;
+        //uint8_t ch = uart_getc(UART_ID);
+        uart_read_blocking(UART_ID, buffer, BUF_LEN);
+        bytes_rxed +=BUF_LEN;
+        
     }
 }
-*/
+
 
 
 static void read_buffer(uint8_t *buf, uint16_t len) {
@@ -53,6 +59,9 @@ int main() {
     gpio_init(LED_PIN);
     gpio_set_dir(LED_PIN, GPIO_OUT);
 
+    // initialising buffer
+    //uint8_t buffer[BUF_LEN];
+
     // Set up our UART with a basic baud rate.
     uart_init(UART_ID, BAUD_RATE);
 
@@ -70,35 +79,38 @@ int main() {
     // Turn off FIFO's - we want to do this character by character
     //uart_set_fifo_enabled(UART_ID, false);
 
+    // Turn ON? FIFO's - we want to read the whole buffer?
+    uart_set_fifo_enabled(UART_ID, true);
+
     // Set up a RX interrupt
     // We need to set up the handler first
     // Select correct interrupt for the UART we are using
-    //int UART_IRQ = UART_ID == uart1 ? UART1_IRQ : UART0_IRQ;
-
+    int UART_IRQ = UART_ID == uart1 ? UART1_IRQ : UART0_IRQ;
+    
     // And set up and enable the interrupt handlers
-    //irq_set_exclusive_handler(UART_IRQ, on_uart_rx);
-    //irq_set_enabled(UART_IRQ, true);
+    irq_set_exclusive_handler(UART_IRQ, on_uart_rx);
+    irq_set_enabled(UART_IRQ, true);
 
     // Now enable the UART to send interrupts - RX only
-    //uart_set_irq_enables(UART_ID, true, false);
+    uart_set_irq_enables(UART_ID, true, false);
     //uart_set_irq_enables(UART_ID, false, false); // no interrupts until we know what it means
 
-    uint8_t buffer[BUF_LEN];
+    
 
     while (1){
         //tight_loop_contents(); // don't know what is this
 
-        read_buffer(buffer, BUF_LEN);
+        printf("%i\n", bytes_rxed);
 
-        if (buffer[0] == 0x20){
-            gpio_put(LED_PIN, 1);
-            sleep_ms(250);
-            gpio_put(LED_PIN, 0);
-            gpio_put(LED_PIN, 1);
-            sleep_ms(250);
-            gpio_put(LED_PIN, 0);
+        //if (buffer[0] == 0x20){
+        gpio_put(LED_PIN, 1);
+        sleep_ms(250);
+        gpio_put(LED_PIN, 0);
+        gpio_put(LED_PIN, 1);
+        sleep_ms(250);
+        gpio_put(LED_PIN, 0);
             
-        }
+        //}
 
         sleep_ms(1000);
 
