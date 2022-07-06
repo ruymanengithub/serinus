b8264378dc852a097d6e36119724d40ab1f0c705
ruyman <ruyman.azzollini@gmail.com>
2022-07-07 00:11:49 +0200

    debugging MPU6050

--- src/MPU6050/mpu6050.h
+++ src/MPU6050/mpu6050.h
@@ -7,8 +7,16 @@
 #include "pico/stdlib.h"
 #include "pico/binary_info.h"
 #include "hardware/i2c.h"
+#include <cmath>
 #define I2C_SDA_PIN 2
 #define I2C_SCL_PIN 3
+#define ACCEL_CONFIG 0x1C
+#define GYRO_CONFIG 0xE0
+
+#define SELF_TEST_X 0x0D
+#define SELF_TEST_Y 0X0E
+#define SELF_TEST_Z 0X0F
+#define SELF_TEST_A 0X10
 
 // By default these devices  are on bus address 0x68
 static int addr = 0x68;  // 104
@@ -26,16 +34,101 @@ void i2c_setup(i2c_inst_t* I2C_ID) {
     // Make the I2C pins available to picotool
     bi_decl(bi_2pins_with_func(I2C_SDA_PIN, I2C_SCL_PIN, GPIO_FUNC_I2C));
 
-    // configure gyro
 
-    uint8_t gyro[] = {0x1B, 0b00011000}; // FS_SEL = 3, 2000 deg/s
+
+    // configure accel.
+    // 0b00010000
+    uint8_t acc[] = {0x1C, 0x18}; // AFS_SEL = 2, 8 g
+    i2c_write_blocking(I2C_ID, addr, acc, 1, true); // true to keep master control of bus
+
+
+    // configure gyro
+    // 0b00010000
+    uint8_t gyro[] = {0x1B, 0x18}; // FS_SEL = 2, 1000 deg/s
     i2c_write_blocking(I2C_ID, addr, gyro, 1, true); // true to keep master control of bus
 
-    uint8_t acc[] = {0x1C, 0b00010000}; // AFS_SEL = 2, 8 g
-    i2c_write_blocking(I2C_ID, addr, acc, 1, true); // true to keep master control of bus
+    
 
 }
 
+void mpu6050_selftest_firstAttempt(i2c_inst_t* I2C_ID, float SelfTest[6]) {
+
+    // self-test accel.
+    // 0b11110000
+    uint8_t acc[] = {0x1C, 0xF8}; // AFS_SEL = 2, 8 g
+    i2c_write_blocking(I2C_ID, addr, acc, 1, true); // true to keep master control of bus
+
+    // configure gyro
+    // 0b11110000
+    uint8_t gyro[] = {0x1B, 0xF8}; // FS_SEL = 2, 1000 deg/s
+    i2c_write_blocking(I2C_ID, addr, gyro, 1, true); // true to keep master control of bus
+
+    sleep_ms(250);
+
+}
+
+uint8_t readByte(i2c_inst_t* I2C_ID, uint8_t subAddress){
+
+    uint8_t dst1 = subAddress;
+    uint8_t data1B;
+    i2c_write_blocking(I2C_ID, addr, &dst1, 1, true);
+    i2c_read_blocking(I2C_ID, addr, &data1B, 1, false);
+    return data1B;
+
+}
+
+void mpu6050_selftest(i2c_inst_t* I2C_ID, float destination[6]) 
+// Should return percent deviation from factory trim values, +/- 14 or less deviation is a pass
+{
+   uint8_t rawData[4];
+   uint8_t selfTest[6];
+   float factoryTrim[6];
+   uint8_t dst2[2];
+
+   // Configure the accelerometer for self-test
+   // 0xF0 = 11110000
+   dst2[0] = ACCEL_CONFIG;
+   dst2[1] = 0xF0;
+   i2c_write_blocking(I2C_ID, addr, dst2, 2, true); // Enable self test on all three axes and set accelerometer range to +/- 8 g
+   // 0xE0 = 11100000
+   dst2[0] = GYRO_CONFIG;
+   dst2[1] = 0xE0;
+   i2c_write_blocking(I2C_ID, addr, dst2, 2, true); // Enable self test on all three axes and set gyro range to +/- 250 degrees/s
+   sleep_ms(250); // Delay a while to let the device execute the self-test
+
+   rawData[0] = readByte(I2C_ID, SELF_TEST_X); // X-axis self-test results
+   rawData[1] = readByte(I2C_ID, SELF_TEST_Y); // Y-axis self-test results
+   rawData[2] = readByte(I2C_ID, SELF_TEST_Z); // Z-axis self-test results
+   rawData[3] = readByte(I2C_ID, SELF_TEST_A); // Mixed-axis self-test results
+   // Extract the acceleration test results first
+   selfTest[0] = (rawData[0] >> 3) | (rawData[3] & 0x30) >> 4 ; // XA_TEST result is a five-bit unsigned integer
+   selfTest[1] = (rawData[1] >> 3) | (rawData[3] & 0x0C) >> 4 ; // YA_TEST result is a five-bit unsigned integer
+   selfTest[2] = (rawData[2] >> 3) | (rawData[3] & 0x03) >> 4 ; // ZA_TEST result is a five-bit unsigned integer
+   // Extract the gyration test results first
+   selfTest[3] = rawData[0]  & 0x1F ; // XG_TEST result is a five-bit unsigned integer
+   selfTest[4] = rawData[1]  & 0x1F ; // YG_TEST result is a five-bit unsigned integer
+   selfTest[5] = rawData[2]  & 0x1F ; // ZG_TEST result is a five-bit unsigned integer   
+   // Process results to allow final comparison with factory set values
+   factoryTrim[0] = (4096.0*0.34)*(pow( (0.92/0.34) , (((float)selfTest[0] - 1.0)/30.0))); // FT[Xa] factory trim calculation
+   factoryTrim[1] = (4096.0*0.34)*(pow( (0.92/0.34) , (((float)selfTest[1] - 1.0)/30.0))); // FT[Ya] factory trim calculation
+   factoryTrim[2] = (4096.0*0.34)*(pow( (0.92/0.34) , (((float)selfTest[2] - 1.0)/30.0))); // FT[Za] factory trim calculation
+   factoryTrim[3] =  ( 25.0*131.0)*(pow( 1.046 , ((float)selfTest[3] - 1.0) ));             // FT[Xg] factory trim calculation
+   factoryTrim[4] =  (-25.0*131.0)*(pow( 1.046 , ((float)selfTest[4] - 1.0) ));             // FT[Yg] factory trim calculation
+   factoryTrim[5] =  ( 25.0*131.0)*(pow( 1.046 , ((float)selfTest[5] - 1.0) ));             // FT[Zg] factory trim calculation
+   
+ //  Output self-test results and factory trim calculation if desired
+ //  Serial.println(selfTest[0]); Serial.println(selfTest[1]); Serial.println(selfTest[2]);
+ //  Serial.println(selfTest[3]); Serial.println(selfTest[4]); Serial.println(selfTest[5]);
+ //  Serial.println(factoryTrim[0]); Serial.println(factoryTrim[1]); Serial.println(factoryTrim[2]);
+ //  Serial.println(factoryTrim[3]); Serial.println(factoryTrim[4]); Serial.println(factoryTrim[5]);
+
+ // Report results as a ratio of (STR - FT)/FT; the change from Factory Trim of the Self-Test Response
+ // To get to percent, must multiply by 100 and subtract result from 100
+   for (int i = 0; i < 6; i++) {
+     destination[i] = 100.0 + 100.0*((float)selfTest[i] - factoryTrim[i])/factoryTrim[i]; // Report percent differences
+   }
+   
+}
 
 void mpu6050_reset(i2c_inst_t* I2C_ID) {
     // Two byte reset. First byte register, second byte data
@@ -79,3 +172,27 @@ void mpu6050_read_raw(i2c_inst_t* I2C_ID, int16_t accel[3], int16_t gyro[3], int
     *temp = buffer[0] << 8 | buffer[1];
 }
 
+void mpu6050_read_cal(i2c_inst_t* I2C_ID, float accel[3], float gyro[3], float *temp) {
+    
+    int16_t rawaccel[3], rawgyro[3], rawtemp;
+
+    mpu6050_read_raw(I2C_ID, rawaccel, rawgyro, &rawtemp);
+
+    // gyro
+
+    for (int i = 0; i < 3; i++) {
+        gyro[i] = rawgyro[i]/32.8; // deg / s 
+    }
+
+    // accel.
+
+    for (int i = 0; i < 3; i++) {
+        accel[i] = rawaccel[i]/4096.0; // g 
+    }
+
+
+    // temperature
+    *temp = (float) (rawtemp / 340.0) + 36.53;
+
+
+}
--- src/MPU6050/mpu6050Demo.c
+++ src/MPU6050/mpu6050Demo.c
@@ -1,6 +1,7 @@
 /**
 * 
 * author: R. Azzollini 
+* 2022
 */
 
 #include <stdio.h>
@@ -13,18 +14,51 @@ int main() {
     
 
     stdio_init_all();
-    printf("Hello, MPU6050! Reading raw data from Registers...\n");
+    printf("\nHello, MPU6050! Reading raw data from Registers...\n");
     
     i2c_setup(I2C_ID);
     mpu6050_reset(I2C_ID);
 
-    int16_t acceleration[3], gyro[3], temp;
+
+    float SelfTest[6];
+
+    mpu6050_selftest(I2C_ID, SelfTest);
+
+    printf("x-axis self test: acceleration trim within : %f pc of factory value\n", SelfTest[0]);
+    printf("y-axis self test: acceleration trim within : %f pc of factory value\n", SelfTest[1]);
+    printf("z-axis self test: acceleration trim within : %f pc of factory value\n", SelfTest[2]);
+    printf("x-axis self test: gyration trim within : %f pc of factory value\n", SelfTest[3]);
+    printf("y-axis self test: gyration trim within : %f pc of factory value\n", SelfTest[4]);
+    printf("z-axis self test: gyration trim within : %f pc of factory value\n", SelfTest[5]);
+
+    /*
+    int16_t racc1[3], rgyro1[3], rtemp;
+    mpu6050_read_raw(I2C_ID, racc1, rgyro1, &rtemp);
+    mpu6050_selftest(I2C_ID);
+    int16_t racc2[3], rgyro2[3];
+    mpu6050_read_raw(I2C_ID, racc2, rgyro2, &rtemp);
+
+    printf("DAcc. X = %d, Y = %d, Z = %d\n", racc2[0], 
+                                    racc2[1], racc2[2]);
+    printf("DGyro. X = %d, Y = %d, Z = %d\n", rgyro2[0], 
+                                    rgyro2[1], rgyro2[2]);
+
+
+    printf("DAcc. X = %d, Y = %d, Z = %d\n", racc2[0]-racc1[0], 
+                                    racc2[1]-racc1[1], racc2[2]-racc2[1]);
+    printf("DGyro. X = %d, Y = %d, Z = %d\n", rgyro2[0]-rgyro1[0], 
+                                    rgyro2[1]-rgyro1[1], rgyro2[2]-rgyro1[2]);
+    */
+
+    float accel[3], gyro[3], temp;
+
+    /*    
 
     while (1) {
         
-        mpu6050_read_raw(I2C_ID, acceleration, gyro, &temp);
+        mpu6050_read_cal(I2C_ID, acceleration, gyro, &temp);
 
-        /*
+        
 
         // These are the raw numbers from the chip, so will need tweaking to be really useful.
         // See the datasheet for more information
@@ -33,12 +67,22 @@ int main() {
         // Temperature is simple so use the datasheet calculation to get deg C.
         // Note this is chip temperature.
         printf("Temp. = %f\n", (temp / 340.0) + 36.53);
-        */
 
-        printf("Gyro. Y = %d\n", gyro[2]);
 
-        sleep_ms(100);
+        //printf("Gyro. Y = %d\n", gyro[2]);
+
+
+        printf("Acc. X = %f, Y = %f, Z = %f\n", accel[0], accel[1], accel[2]);
+        printf("Gyro. X = %f, Y = %f, Z = %f\n", gyro[0], gyro[1], gyro[2]);
+        printf("Temp. = %f\n", temp);
+
+
+        sleep_ms(500);
+
     }
+    */
+
+
 //#endif
     return 0;
 }
